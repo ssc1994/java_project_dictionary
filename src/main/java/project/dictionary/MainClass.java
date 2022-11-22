e7ab877a272d8a47d6757eb5e169864d328a6ea6
user <user@DESKTOP-3RJ4SCN>
2022-11-22 19:51:30 +0900

    20221122
    jaeyun

--- src/main/java/project/dictionary/MainClass.java
+++ src/main/java/project/dictionary/MainClass.java
@@ -1,7 +1,12 @@
 package project.dictionary;
 
 public class MainClass {
-	
-	
+	public static void main(String[] args) {
+		
+		ddb d = new ddb();
+		d.getConnection();
+		d.createTables();
+		
+	}
 	
 }
