291fe65ddb23ca36f4433fbdb1ab693849565bd1
박희진 <116553094+heejininging@users.noreply.github.com>
2022-11-23 00:21:22 +0900

    Update MakeMemo.java

--- src/main/java/project/dictionary/MakeMemo.java
+++ src/main/java/project/dictionary/MakeMemo.java
@@ -9,6 +9,7 @@ public class MakeMemo {
 
 		data.getConnection();
 		
+		//테이블 생성문
 		try {
 			data.createTables();
 			System.out.println("테이블 생성완료");
@@ -18,9 +19,30 @@ public class MakeMemo {
 			System.out.println("테이블 미생성");
 		}
 		
+		//단어 단어장에 추가하기
 		data.insertWord("안녕하세요", "인삿말",1);
 		
+		//단어장에 있는 단어목록
 		data.showTable();
+
+		/*
+		 * 단어장과 어울리는 테이블 구상
+		 * 
+		 * 1. 단어장에 있는 단어들에 대한 문제 맞추기 테이블
+		 * (낱말보고 단어 맞추기, 의미보고 단어 맞추기)
+		 * 
+		 * 2. 수준별로 (중요도) 에 따라 컬럼별로 나눈 테이블
+		 * (예를 들면 초등, 중등, 고등처럼 수준별로)
+		 * 
+		 * 3. 단어 중에 마음에 드는 단어나 복습하고 싶은 단어가 생기면
+		 * 나만의 단어장에 저장하는 테이블
+		 * 
+		 * 4. 
+		 * 
+		 */
+	
 	}
 	
 }
+
+
