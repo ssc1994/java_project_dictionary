1d22a962aef580596d446c65c3d3352af0df4fc2
user <user@DESKTOP-F6VNPM7>
2022-11-24 18:36:29 +0900

    11/24 18:36
    jaewon final

--- src/main/java/project/dictionary/UserDAO.java
+++ src/main/java/project/dictionary/UserDAO.java
@@ -69,26 +69,26 @@ public class UserDAO { //현재 로그인 멤버 구현
 		return currMember;
 	}
 	
-//	public MemberVO loginMember() {
-//		MemberVO currMember = new MemberVO();
-//		Scanner sc = new Scanner(System.in);
-//		
-//		try {
-//			UserDAO dao = new UserDAO();
-//			System.out.print(">");
-//			String memberID = sc.next();
-//			System.out.print(">");
-//			String memberPassword = sc.next();
-//			if(dao.login(memberID, memberPassword) == 1) {
-//				currMember.setName(memberID);
-//				System.out.println("로그인 성공");
-//			} else {
-//				System.out.println("실패");
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return currMember;
-//	}
+	public MemberVO loginMember() {
+		MemberVO currMember = new MemberVO();
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			UserDAO dao = new UserDAO();
+			System.out.print(">");
+			String memberID = sc.next();
+			System.out.print(">");
+			String memberPassword = sc.next();
+			if(dao.login(memberID, memberPassword) == 1) {
+				currMember.setName(memberID);
+				System.out.println("로그인 성공");
+			} else {
+				System.out.println("실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return currMember;
+	}
 	
 }
